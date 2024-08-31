a3c2ce49c962afa33a142bc29e4ca88689ed46b9
Vasiliy Yalovchik <vasyabylba@gmail.com>
2024-09-01 02:44:04 +0300

    add VideoCard class

---
+++ src/main/java/by/vasyabylba/VideoCard.java
@@ -0,0 +1,47 @@
+package by.vasyabylba;
+
+public class VideoCard {
+    private String brand;
+    private String model;
+    private int vramInGB;
+    private double coreClockMHz;
+
+    public VideoCard(String brand, String model, int vramInGB, double coreClockMHz) {
+        this.brand = brand;
+        this.model = model;
+        this.vramInGB = vramInGB;
+        this.coreClockMHz = coreClockMHz;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getVramInGB() {
+        return vramInGB;
+    }
+
+    public void setVramInGB(int vramInGB) {
+        this.vramInGB = vramInGB;
+    }
+
+    public double getCoreClockMHz() {
+        return coreClockMHz;
+    }
+
+    public void setCoreClockMHz(double coreClockMHz) {
+        this.coreClockMHz = coreClockMHz;
+    }
+}
