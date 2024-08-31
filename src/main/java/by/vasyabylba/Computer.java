fb64794cb1f4582a02955869b9054cefdca5398b
Vasiliy Yalovchik <vasyabylba@gmail.com>
2024-09-01 02:20:02 +0300

    Add Computer class

---
+++ src/main/java/by/vasyabylba/Computer.java
@@ -0,0 +1,47 @@
+package by.vasyabylba;
+
+public class Computer {
+    private String brand;
+    private String model;
+    private int ramSizeInGB;
+    private double processorSpeedGHz;
+
+    public Computer(String brand, String model, int ramSizeInGB, double processorSpeedGHz) {
+        this.brand = brand;
+        this.model = model;
+        this.ramSizeInGB = ramSizeInGB;
+        this.processorSpeedGHz = processorSpeedGHz;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getRamSizeInGB() {
+        return ramSizeInGB;
+    }
+
+    public void setRamSizeInGB(int ramSizeInGB) {
+        this.ramSizeInGB = ramSizeInGB;
+    }
+
+    public double getProcessorSpeedGHz() {
+        return processorSpeedGHz;
+    }
+
+    public void setProcessorSpeedGHz(double processorSpeedGHz) {
+        this.processorSpeedGHz = processorSpeedGHz;
+    }
+}
