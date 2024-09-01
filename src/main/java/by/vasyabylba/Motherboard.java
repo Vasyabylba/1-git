7792ec68e479691cdced749b47a889fd13976ff5
Vasiliy Yalovchik <vasyabylba@gmail.com>
2024-09-01 17:08:18 +0300

    add ramSlots field to toString() method

--- src/main/java/by/vasyabylba/Motherboard.java
+++ src/main/java/by/vasyabylba/Motherboard.java
@@ -61,6 +61,7 @@ public class Motherboard {
                 "brand='" + brand + '\'' +
                 ", model='" + model + '\'' +
                 ", chipset='" + chipset + '\'' +
+                ", ramSlots=" + ramSlots +
                 '}';
     }
 }
