0d8b67a0e4d7f2acdd815d75a528acd880e51add
Vasiliy Yalovchik <vasyabylba@gmail.com>
2024-09-01 17:11:28 +0300

    add CPU class

---
+++ src/main/java/by/vasyabylba/CPU.java
@@ -0,0 +1,47 @@
+package by.vasyabylba;
+
+public class CPU {
+    private String brand;
+    private String model;
+    private int cores;
+    private double clockSpeedGHz;
+
+    public CPU(String brand, String model, int cores, double clockSpeedGHz) {
+        this.brand = brand;
+        this.model = model;
+        this.cores = cores;
+        this.clockSpeedGHz = clockSpeedGHz;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getCores() {
+        return cores;
+    }
+
+    public void setCores(int cores) {
+        this.cores = cores;
+    }
+
+    public double getClockSpeedGHz() {
+        return clockSpeedGHz;
+    }
+
+    public void setClockSpeedGHz(double clockSpeedGHz) {
+        this.clockSpeedGHz = clockSpeedGHz;
+    }
+}
