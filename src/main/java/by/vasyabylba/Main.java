bfb05e011b641268578d012fb924acbd13a6df04
Vasiliy Yalovchik <vasyabylba@gmail.com>
2024-09-01 03:11:47 +0300

    welcome message set as constant

--- src/main/java/by/vasyabylba/Main.java
+++ src/main/java/by/vasyabylba/Main.java
@@ -1,7 +1,9 @@
 package by.vasyabylba;
 
 public class Main {
+    private static final String WELCOME_MESSAGE = "Hello world!";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(WELCOME_MESSAGE);
     }
 }
